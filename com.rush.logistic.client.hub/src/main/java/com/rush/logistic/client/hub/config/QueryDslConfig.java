0dddf32fa7ea1e6c9ebe7dd98047a9055e854e6f
Hxxseung <soaorsith2@naver.com>
2024-12-18 19:50:41 +0900

    :speech_balloon: add : text add for study

--- com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/config/QueryDslConfig.java
+++ com.rush.logistic.client.hub/src/main/java/com/rush/logistic/client/hub/config/QueryDslConfig.java
@@ -13,7 +13,8 @@ public class QueryDslConfig {
     private final EntityManager entityManager;
 
     @Bean
-    public JPAQueryFactory jpaQueryFactory(){
+    public JPAQueryFactory jpaQueryFactory(){   //데이터베이스 쿼리를 작성할 수 있도록 제공,JPAQueryFactory를 Bean으로 등록
+                                                //QueryDSL을 통해 안전한 쿼리를 작성할 수 있도록 제공.
         return new JPAQueryFactory(entityManager);
     }
 }
